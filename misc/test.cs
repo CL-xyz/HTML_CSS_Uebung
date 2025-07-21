9d19649899ee2d1e33e43aafbb0c583f450ae36d
CL-xyz <lprchristoph@gmail.com>
2025-07-21 12:31:49 +0200

    Add files via upload

---
+++ misc/test.cs
@@ -0,0 +1,14 @@
+//int Testwert1();
+
+public partial class HalloWeltForm : Form
+{
+    public HalloWeltForm()
+    {
+        InitializeComponent();
+    }
+
+    private void OK_Click(object sender, EventArgs e)
+    {
+        MessageBox.Show("HalloWelt");
+    }
+}
